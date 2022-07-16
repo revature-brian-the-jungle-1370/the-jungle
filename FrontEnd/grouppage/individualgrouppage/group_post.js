afe5395b56a016680f74e2afb747d8cbe5018539
Andrew Dang <{ID}+{username}@users.noreply.github.com>
2022-07-16 10:16:37 -0400

    edited group_post.js

--- FrontEnd/grouppage/individualgrouppage/group_post.js
+++ FrontEnd/grouppage/individualgrouppage/group_post.js
@@ -10,10 +10,10 @@ async function createGroupPost() {
   let post_text = document.getElementById("postInput").value;
   let data = {
     "post_id": "0",
-    // "user_id": Number(sessionStorage.getItem("user_id")),
-    // "group_id": Number(sessionStorage.getItem("group_id")),
-    "user_id": 9000,
-    "group_id": 9000,
+    "user_id": Number(sessionStorage.getItem("user_id")),
+    "group_id": Number(sessionStorage.getItem("group_id")),
+    // "user_id": 10000,
+    // "group_id": 9000,
     "post_text": post_text,
     "image_data": "",
     "likes": 0,
