d7eddb7ec00ccd2f4702829d118840b7fd7c7693
Andrew Dang <{ID}+{username}@users.noreply.github.com>
2022-07-16 15:07:23 -0400

    experimenting with group_post.js

--- FrontEnd/grouppage/individualgrouppage/group_post.js
+++ FrontEnd/grouppage/individualgrouppage/group_post.js
@@ -1,4 +1,5 @@
-let url = "http://ec2-52-200-53-62.compute-1.amazonaws.com:5000/";
+let python_url =  "http://ec2-52-200-53-62.compute-1.amazonaws.com:5000/";
+let java_url =    "http://ec2-52-200-53-62.compute-1.amazonaws.com:8080/";
 
 let tableBody = document.getElementById("postBody");
 
@@ -7,11 +8,14 @@ let tableBody = document.getElementById("postBody");
 document.getElementById("sendGroupPostButton").addEventListener("click", createGroupPost);
 
 async function createGroupPost() {
+  localStorage.setItem("user_id", 13)
+  localStorage.setItem("group_id", 10000) //Comment this out later. This is to test functionality
+  
   let post_text = document.getElementById("postInput").value;
   let data = {
     "post_id": "0",
-    "user_id": Number(sessionStorage.getItem("user_id")),
-    "group_id": Number(sessionStorage.getItem("group_id")),
+    "user_id": Number(localStorage.getItem("user_id")),
+    "group_id": Number(localStorage.getItem("group_id")),
     // "user_id": 10000,
     // "group_id": 9000,
     "post_text": post_text,
@@ -19,72 +23,74 @@ async function createGroupPost() {
     "likes": 0,
     "date_time_of_creation": ""
   }
+  console.log("localStorage.getItem(\"user_id\"): " + Number(localStorage.getItem("user_id")))
+  console.log("localStorage.getItem(\"group_id\"): " + Number(localStorage.getItem("group_id")))
 
-  let response = await fetch(url + "/group_post", {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-      'Accept': 'application/json'
-    },
-    redirect: "follow",
-    body: JSON.stringify(data),
-  });
+  // let response = await fetch(python_url + "/group_post", {
+  //   method: "POST",
+  //   mode: "cors",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     'Accept': 'application/json'
+  //   },
+  //   redirect: "follow",
+  //   body: JSON.stringify(data),
+  // });
 
-  let responseBody = await response.json();
-  if (response.status == 201) {
+  // let responseBody = await response.json();
+  // if (response.status == 201) {
 
-    console.log(responseBody);
-    // document.getElementById("postInfo").innerHTML = `Post sent`
-    document.getElementById("postInfo").innerHTML = `
-      <div class="overlap-group1" id="headingNew${post.post_id}">
-      <p> ` + post_id + `</p>
-      <p> ` + user_id + `</p>
-      <p> ` + post_text + `</p>
-      <p> Likes: ` + likes + `</p>
-      <p> ` + date_time_of_creation + `</p>
-      <button id="deletePost${post.post_id}" onclick="deletePost(${post.post_id})">Delete</button>
-      </div>
-        `;
-    thePost = `
-    <div class="flex-row">
-                <div class="overlap-group">
-                  <div class="new-york-ny valign-text-middle poppins-bold-pink-swan-14px">New York, NY</div>
-                  <div class="username-1 valign-text-middle poppins-bold-cape-cod-20px">JostSNL21</div>
-                  <img class="feed-avatar" src="img/feed-avatar-1@2x.png" />
-                </div>
-                <img class="three-dots-icon" src="img/three-dots-icon-1@2x.svg"/>
-              </div>
-              <img class="feed-picture" src="img/feed-picture@2x.svg" />
-              <div class="icon-container">
-                <img class="heart-icon" src="img/heart-icon@2x.svg" />
-                <img
-                  class="chat-bubble-icon"
-                  src="img/chat-bubble-icon@2x.svg"
-                />
-                <img class="share-icon" src="img/share-icon@2x.svg" />
-              </div>
-              <div class="overlap-group2">
-                <div
-                  class="feed-text-2 valign-text-middle poppins-medium-black-18px"
-                >
-                  At the beach with bae
-                </div>
-                <div
-                  class="username-2 valign-text-middle poppins-bold-cape-cod-20px"
-                >
-                  JostSNL21
-        </div>
-      </div>`
-    document.getElementById("postInfo").innerHTML = thePost
-  } else {
-    document.getElementById("postInfo").innerHTML = `Post could not be sent`
-  }
+  //   console.log(responseBody);
+  //   // document.getElementById("postInfo").innerHTML = `Post sent`
+  //   document.getElementById("postInfo").innerHTML = `
+  //     <div class="overlap-group1" id="headingNew${post.post_id}">
+  //     <p> ` + post_id + `</p>
+  //     <p> ` + user_id + `</p>
+  //     <p> ` + post_text + `</p>
+  //     <p> Likes: ` + likes + `</p>
+  //     <p> ` + date_time_of_creation + `</p>
+  //     <button id="deletePost${post.post_id}" onclick="deletePost(${post.post_id})">Delete</button>
+  //     </div>
+  //       `;
+  //   thePost = `
+  //   <div class="flex-row">
+  //               <div class="overlap-group">
+  //                 <div class="new-york-ny valign-text-middle poppins-bold-pink-swan-14px">New York, NY</div>
+  //                 <div class="username-1 valign-text-middle poppins-bold-cape-cod-20px">JostSNL21</div>
+  //                 <img class="feed-avatar" src="img/feed-avatar-1@2x.png" />
+  //               </div>
+  //               <img class="three-dots-icon" src="img/three-dots-icon-1@2x.svg"/>
+  //             </div>
+  //             <img class="feed-picture" src="img/feed-picture@2x.svg" />
+  //             <div class="icon-container">
+  //               <img class="heart-icon" src="img/heart-icon@2x.svg" />
+  //               <img
+  //                 class="chat-bubble-icon"
+  //                 src="img/chat-bubble-icon@2x.svg"
+  //               />
+  //               <img class="share-icon" src="img/share-icon@2x.svg" />
+  //             </div>
+  //             <div class="overlap-group2">
+  //               <div
+  //                 class="feed-text-2 valign-text-middle poppins-medium-black-18px"
+  //               >
+  //                 At the beach with bae
+  //               </div>
+  //               <div
+  //                 class="username-2 valign-text-middle poppins-bold-cape-cod-20px"
+  //               >
+  //                 JostSNL21
+  //       </div>
+  //     </div>`
+  //   document.getElementById("postInfo").innerHTML = thePost
+  // } else {
+  //   document.getElementById("postInfo").innerHTML = `Post could not be sent`
+  // }
 }
 
 //--------------------------------------------------- LOAD GROUP POST FUNCTION-------------------------------------------------------
 async function getPost() {
-  let response = await fetch(url + "/group_post/group/9000", { //replace with "/group_post/group/" + group_id
+  let response = await fetch(python_url + "/group_post/group/10000", { //replace with "/group_post/group/" + group_id
     method: "GET",
     mode: "cors",
   });
@@ -94,13 +100,13 @@ async function getPost() {
   }
 }
 
-  function populateData(responseBody) {
+async function populateData(responseBody) {
   const allpost = document.getElementById("allpost");
   for (let post of responseBody) {
-    let Date = post.date_time_of_creation.slice(0, -7);
+    let date_1 = post.date_time_of_creation.slice(0, -7);
     let postBox = document.createElement('div');
     postBox.innerHTML = `
-    <p style=" float:right;font-size: 1em;color:grey;"> ` + Date + `</p>
+    <p style=" float:right;font-size: 1em;color:grey;"> ` + date_1 + `</p>
     <div class="overlap-group1" id="newPost${post.post_id}">
 
     <span style="font-weight: bold; margin-right: 18em; margin-top: 1em; font-size: 1.2em;"><p> ` + post.user_id + `</p></span>
@@ -119,72 +125,72 @@ async function getPost() {
   </div>
 
     <button id="deletePost${post.post_id}" onclick="deleteGroupPost(${post.post_id})" class="btn btn-sm btn-danger" style="margin-top: 10em;"><i class="fa fa-trash"></i> Delete</button>
+    </div>
+    <div class="overlap-group1" id="newPost${post.post_id}">
+    <p> ` + post.post_id + `</p>
+    <p> ` + post.user_id + `</p>
+    <p> ` + post.post_text + `</p>
+    <p> Likes: ` + post.likes + `</p>
+    <p> ` + post.date_time_of_creation + `</p>
+    <button id="deletePost${post.post_id}" onclick="deleteGroupPost(${post.post_id})">Delete</button>
     </div>`
-    // <div class="overlap-group1" id="newPost${post.post_id}">
-    // <p> ` + post.post_id + `</p>
-    // <p> ` + post.user_id + `</p>
-    // <p> ` + post.post_text + `</p>
-    // <p> Likes: ` + post.likes + `</p>
-    // <p> ` + post.date_time_of_creation + `</p>
-    // <button id="deletePost${post.post_id}" onclick="deleteGroupPost(${post.post_id})">Delete</button>
-    // </div>`
 
-  //   //add the poster image
-  //   let url = "http://127.0.0.1:5000/user/image/" + post.user_id;
-  //   let response = await fetch(url);
-  //   let user_image_text;
-  // if(response.status === 200){
-  //     user_image_text = await response.text();}
+    //add the poster image
+    let url = python_url + "user/image/" + post.user_id;
+    let response = await fetch(url);
+    let user_image_text;
+    if(response.status === 200){
+        user_image_text = await response.text();}
 
-  //   //get the post image
-  //   url = "http://127.0.0.1:5000/post/image/" + post.post_id;
-  //   console.log(url);
-  //   response = await fetch(url);
-  //   console.log(response);
-  //   let date_time = new Date(post.date_time_of_creation)
-  //   let date = date_time.toDateString();
+    //get the post image
+    url = python_url + "post/image/" + post.post_id;
+    console.log(url);
+    response = await fetch(url);
+    console.log(response);
+    let date_time = new Date(post.date_time_of_creation)
+    let date_2 = date_time.toDateString();
 
-  //   if(response.status === 200){
-  //     const image_text = await response.text();
-  //     postBox.innerHTML =
-  //   `<div class="flex-row">
-  //     <div class="overlap-group">
-  //     <div class="new-york-ny valign-text-middle poppins-bold-pink-swan-14px"> `+ date +` </div>
-  //       <div class="username-1 valign-text-middle poppins-bold-cape-cod-20px">JostSNL21</div>
-  //         <img class="feed-avatar" src="`+ user_image_text + `" alt="img/feed-avatar-1@2x.png" />
-  //       </div>
-  //     <input type="image" class="three-dots-icon" src="img/three-dots-icon-1@2x.svg" id="deletePost${post.post_id}" onclick="deleteGroupPost(${post.post_id})"/>
-  //     </div>
-  //     <img class="feed-picture" src="`+ image_text +`"/>
-  //     <div class="icon-container">
-  //       <input type="image" class="heart-icon" src="img/heart-icon@2x.svg" />
-  //       <p>` + post.likes + `</p>
-  //       <input type="image" class="chat-bubble-icon" src="img/chat-bubble-icon@2x.svg"/>
-  //       <img class="share-icon" src="img/share-icon@2x.svg" />
-  //     </div>
-  //     <div class="overlap-group2">
-  //       <div class="feed-text-2 valign-text-middle poppins-medium-black-18px">`+ post.post_text +`</div>
-  //   </div>`
-  //   }else{
-  //     postBox.innerHTML =
-  //   `<div class="flex-row">
-  //     <div class="overlap-group">
-  //     <div class="new-york-ny valign-text-middle poppins-bold-pink-swan-14px"> `+ date +` </div>
-  //       <div class="username-1 valign-text-middle poppins-bold-cape-cod-20px">JostSNL21</div>
-  //         <img class="feed-avatar" id="UserImage`+ post.post_id +`" src="img/feed-avatar-1@2x.png" />
-  //       </div>
-  //     <input type="image" class="three-dots-icon" src="img/three-dots-icon-1@2x.svg" id="deletePost${post.post_id}" onclick="deleteGroupPost(${post.post_id})"/>
-  //     </div>
-  //     <div class="icon-container">
-  //       <input type="image" class="heart-icon" src="img/heart-icon@2x.svg" />
-  //       <p>` + post.likes + `</p>
-  //       <input type="image" class="chat-bubble-icon" src="img/chat-bubble-icon@2x.svg"/>
-  //       <img class="share-icon" src="img/share-icon@2x.svg" />
-  //     </div>
-  //     <div class="overlap-group2">
-  //       <div class="feed-text-2 valign-text-middle poppins-medium-black-18px">`+ post.post_text +`</div>
-  //   </div>`
-  //   }
+    if(response.status === 200){
+      const image_text = await response.text();
+      postBox.innerHTML =
+    `<div class="flex-row">
+      <div class="overlap-group">
+      <div class="new-york-ny valign-text-middle poppins-bold-pink-swan-14px"> `+ date_2 +` </div>
+        <div class="username-1 valign-text-middle poppins-bold-cape-cod-20px">JostSNL21</div>
+          <img class="feed-avatar" src="`+ user_image_text + `" alt="img/feed-avatar-1@2x.png" />
+        </div>
+      <input type="image" class="three-dots-icon" src="img/three-dots-icon-1@2x.svg" id="deletePost${post.post_id}" onclick="deleteGroupPost(${post.post_id})"/>
+      </div>
+      <img class="feed-picture" src="`+ image_text +`"/>
+      <div class="icon-container">
+        <input type="image" class="heart-icon" src="img/heart-icon@2x.svg" />
+        <p>` + post.likes + `</p>
+        <input type="image" class="chat-bubble-icon" src="img/chat-bubble-icon@2x.svg"/>
+        <img class="share-icon" src="img/share-icon@2x.svg" />
+      </div>
+      <div class="overlap-group2">
+        <div class="feed-text-2 valign-text-middle poppins-medium-black-18px">`+ post.post_text +`</div>
+    </div>`
+    }else{
+      postBox.innerHTML =
+    `<div class="flex-row">
+      <div class="overlap-group">
+      <div class="new-york-ny valign-text-middle poppins-bold-pink-swan-14px"> `+ date_2 +` </div>
+        <div class="username-1 valign-text-middle poppins-bold-cape-cod-20px">JostSNL21</div>
+          <img class="feed-avatar" id="UserImage`+ post.post_id +`" src="img/feed-avatar-1@2x.png" />
+        </div>
+      <input type="image" class="three-dots-icon" src="img/three-dots-icon-1@2x.svg" id="deletePost${post.post_id}" onclick="deleteGroupPost(${post.post_id})"/>
+      </div>
+      <div class="icon-container">
+        <input type="image" class="heart-icon" src="img/heart-icon@2x.svg" />
+        <p>` + post.likes + `</p>
+        <input type="image" class="chat-bubble-icon" src="img/chat-bubble-icon@2x.svg"/>
+        <img class="share-icon" src="img/share-icon@2x.svg" />
+      </div>
+      <div class="overlap-group2">
+        <div class="feed-text-2 valign-text-middle poppins-medium-black-18px">`+ post.post_text +`</div>
+    </div>`
+    }
 
     allpost.appendChild(postBox)
   }
@@ -195,7 +201,7 @@ getPost()
 //--------------------------------------------------- DELETE GROUP POST FUNCTION-------------------------------------------------------
 
 async function deleteGroupPost(post_id) {
-  let response = await fetch(url + "/group_post/" + post_id, {
+  let response = await fetch(url + "group_post/" + post_id, {
     method: "DELETE",
     mode: "cors"
   });
