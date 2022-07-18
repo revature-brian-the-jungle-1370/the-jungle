f426956e35cf33d44a10bf8c9626fad58085d79c
Modesto Amador <mofesdo.dev@gmail.com>
2022-07-18 13:37:59 -0700

    happy and sad paths done

--- PythonAPI/service_layer/implementation_classes/comment_service.py
+++ PythonAPI/service_layer/implementation_classes/comment_service.py
@@ -4,6 +4,10 @@ from data_access_layer.implementation_classes.comment_dao import CommentDAOImp
 from entities.comment import Comment
 from entities.returned_comment import ReturnedComment
 from service_layer.abstract_classes.comment_service_abs import CommentService
+from custom_exceptions.comment_id_must_be_an_integer import CommentIdMustBeAnInteger
+from custom_exceptions.post_id_must_be_an_integer import PostIdMustBeAnInteger
+from custom_exceptions.comment_must_be_a_string import CommentMustBeAString
+from custom_exceptions.comment_not_found import CommentNotFound
 
 
 class CommentServiceImp(CommentService):
@@ -12,10 +16,24 @@ class CommentServiceImp(CommentService):
 
     def service_create_comment(self, post_id: int, user_id: int, comment_text: str, group_id: int,
                                reply_user: int) -> Comment:
+                               
+        # Check to make sure the user_id is an integer
+        if not str(post_id).isnumeric():
+            raise PostIdMustBeAnInteger('The Post ID must be an integer.')
+        post_id = int(post_id)
+
+        # Check to make sure the text is a string.
+        if not type(comment_text) == str:
+            raise CommentMustBeAString("The comment text must be a string.")
+
         return self.comment_dao.create_comment(post_id, user_id, comment_text, group_id, reply_user)
 
     def service_get_comment_by_post_id(self, post_id: int) -> List[ReturnedComment]:
         return self.comment_dao.get_comment_by_post_id(post_id)
 
     def service_delete_comment(self, comment_id: int) -> bool:
+        # Check to make sure the user_id is an integer
+        if not str(comment_id).isnumeric():
+            raise CommentIdMustBeAnInteger('The Comment ID must be an integer.')
+        comment_id = int(comment_id)
         return self.comment_dao.delete_comment(comment_id)
--- PythonAPI/tests_for_app/test_for_service_layer/test_comment_service.py
+++ PythonAPI/tests_for_app/test_for_service_layer/test_comment_service.py
@@ -18,26 +18,34 @@ def test_service_create_comment_success():
     comment_dao.create_comment = MagicMock(return_value="thisisareturnvalue")
     assert comment_service.service_create_comment(post_id= 1000, user_id= 1000, comment_text="success", group_id=1000, reply_user=1000)
 
-# def test_service_create_comment_failure_comment_no_int():
-#     comment_dao.create_comment = MagicMock(return_value="thisisareturnvalue")
-#     try:
-#         comment_service.service_create_comment(post_id= 1000, user_id= 1000, comment_text=1000, group_id=1000, reply_user=1000)
-#         assert False
-#     except CommentMustBeAString as e:
-#         assert str(e) == "Comment must be a string."
+def test_service_create_comment_failure_comment_no_int():
+    comment_dao.create_comment = MagicMock(return_value="thisisareturnvalue")
+    try:
+        comment_service.service_create_comment(post_id= 1000, user_id= 1000, comment_text=100.0, group_id=1000, reply_user=1000)
+        assert False
+    except CommentMustBeAString as e:
+        assert str(e) == "The comment text must be a string."
 
 def test_service_get_comment_by_post_id_success():
     comment_dao.get_comment_by_post_id = MagicMock(return_value="thisisareturnvalue")
     assert comment_service.service_get_comment_by_post_id(1000)
 
-# def test_service_get_comment_by_post_id_failure_post_id_not_int():
-#     comment_dao.create_comment = MagicMock(return_value="thisisareturnvalue")
-#     try:
-#         comment_service.service_create_comment(post_id= 100.4, user_id= 1000, comment_text="failure", group_id=1000, reply_user=1000)
-#         assert False
-#     except PostIdMustBeAnInteger as e:
-#         assert str(e) == "Post ID must be an integer."   
+def test_service_get_comment_by_post_id_failure_post_id_not_int():
+    comment_dao.create_comment = MagicMock(return_value="thisisareturnvalue")
+    try:
+        comment_service.service_create_comment(post_id= 100.4, user_id= 1000, comment_text="failure", group_id=1000, reply_user=1000)
+        assert False
+    except PostIdMustBeAnInteger as e:
+        assert str(e) == "The Post ID must be an integer."   
 
 def test_service_delete_comment_success():
     comment_dao.delete_comment = MagicMock(return_value="thisisareturnvalue")
     assert comment_service.service_delete_comment(1000)
+
+def test_service_delete_comment_failure_comment_id_not_int():
+    comment_dao.delete_comment = MagicMock(return_value="thisisareturnvalue")
+    try:
+        comment_service.service_delete_comment(100.1)
+        assert False
+    except CommentIdMustBeAnInteger as e:
+        assert str(e) == "The Comment ID must be an integer."  
