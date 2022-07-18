4d009c999947dd408182835eb8d26141297f5651
Andrew Dang <{ID}+{username}@users.noreply.github.com>
2022-07-18 11:49:17 -0400

    frondEndURL added /FrontEnd

--- FrontEnd/loginpage/login.js
+++ FrontEnd/loginpage/login.js
@@ -8,7 +8,7 @@ const invalidIcon = document.querySelectorAll("[id='invalid-icon']");
 let invalidMessage = document.querySelectorAll("[id='signup-invalid-message']");
 let infoIcon = document.querySelectorAll(".info-icon");
 const url = "http://127.0.0.1:8080";
-const frontendUrl="http://127.0.0.1:5500";
+const frontendUrl="http://127.0.0.1:5500/FrontEnd";
 let validateCounter = 0;
 
 const div = document.getElementById("errorMessageGoesHere");
