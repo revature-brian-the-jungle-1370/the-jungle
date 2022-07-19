948af231a7cbe151d30931ab2fb8605b0060bcb2
Sachin Kumar <sachinkgarg6@gmail.com>
2022-07-19 14:49:48 -0400

    ReactToAPost

--- PythonAPI/tests_for_app/test_for_data_access_layer/test_likes_dao.py
+++ PythonAPI/tests_for_app/test_for_data_access_layer/test_likes_dao.py
@@ -1,12 +1,30 @@
 from data_access_layer.implementation_classes.like_post_dao import LikePostDaoImp
+from data_access_layer.implementation_classes.comment_dao import CommentDAOImp
+from custom_exceptions.connection_error import ConnectionErrorr
+
 
 like_post_dao = LikePostDaoImp()
+commentObj= CommentDAOImp()
 
-# #Happy Path Testing
-# def test_like_post_success1():
-#     assert like_post_dao.like_post(1) > 2
-#
-#
-# #Happy Path Testing
-# def test_like_post_success2():
-#     assert like_post_dao.like_post(4) > 3
+
+def test_like_comment_success():
+    comments=commentObj.get_comment_by_post_id(230)
+    # for comment in comments:
+    likes=comments[0].likes
+    liked_comment=like_post_dao.like_comment(comments[0].comment_id)
+    assert liked_comment>0
+
+
+def test_like_comment_invalid_postId():
+    try:
+        like_post_dao.like_comment(5)
+        assert False
+    except ConnectionErrorr as e:
+        str(e) == "post not found"
+
+def test_like_comment_invalid_postId2():
+    try:
+        like_post_dao.like_comment("5")
+        assert False
+    except ConnectionErrorr as e:
+        str(e) == "post not found"
