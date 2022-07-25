1f5724b814f57f440d343415988e74f4747c0f9b
danscode <dankim.dk1@gmail.com>
2022-07-25 12:51:21 -0400

    changed localstorage call

--- FrontEnd/profilepage/js/followers-&-about-me.js
+++ FrontEnd/profilepage/js/followers-&-about-me.js
@@ -80,7 +80,7 @@ function successMessageForProfileModal(){
 }
 
 async function getUserFollowers(){
-    let userId= localStorage("userId");
+    let userId= localStorage.getItem("userId");
     let url = "http://ec2-52-200-53-62.compute-1.amazonaws.com:5000/user/followers/"+userId;
 
     let response = await fetch(url);
