d38faf20d295570284a66d6620b7347747fa8bb1
danscode <dankim.dk1@gmail.com>
2022-07-25 12:26:32 -0400

    changed chatpage.js

--- FrontEnd/chatpage/js/chat-page.js
+++ FrontEnd/chatpage/js/chat-page.js
@@ -1,7 +1,7 @@
 // small helper function for selecting element by id
 let id = (id) => document.getElementById(id);
 
-let linkstring = "http://ec2-52-200-53-62.compute-1.amazonaws.com";
+let linkstring = "ec2-52-200-53-62.compute-1.amazonaws.com";
 
 storage = JSON.parse(localStorage.getItem("userInfo"));
 
