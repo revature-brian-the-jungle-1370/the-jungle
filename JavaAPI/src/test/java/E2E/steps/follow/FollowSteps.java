6fe1e5e996b9bcc59c6936fe4757596a46709753
John Orellano <johnorellano@gmail.com>
2022-07-25 10:52:40 -0400

    deletion

--- JavaAPI/src/test/java/E2E/steps/follow/FollowSteps.java
+++ JavaAPI/src/test/java/E2E/steps/follow/FollowSteps.java
@@ -59,29 +59,4 @@ public class FollowSteps {
         TestRunner.userProfile.unfollowUserButton.click();
     }
 
-/*     @Then("the user will be redirected to their feed page")
-    public void the_user_will_be_redirected_back_to_their_feed_page(){
-        //explicit wait for title?
-        //String title = new TestRunner.driver.getTitle();
-        //Assert.assertEquals(title, "");
-    } */
-
-    //Scenario 3 As a User I want to see my followers on the feed page
-    //MAY CHANGE DEPENDING ON FUNCTIONALITY
-    @Given("the user is logged in on homepage")
-    public void the_user_is_logged_in_on_homepage() {
-
-    }
-
-    @When("the user clicks on their feed page")
-    public void the_user_clicks_on_their_feed_page(){
-
-    }
-
-    @Then("the user will be redirected to the feed page and see their followers")
-    public void the_user_will_be_redirected_to_their_feed_and_see_their_followers(){
-
-    }
-    
-
 }
